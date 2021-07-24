404a1c43fec7fc931dcc7c4e481376402cc16bbe
sg-i <sgadov2vanya@yandex.ru>
2021-07-24 13:10:38 +0300

    start project

---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+import  {createTheme}  from "@material-ui/core/styles";
+import green from '@material-ui/core/colors/green';
+export const theme = createTheme({
+    palette:{
+        primary: {
+            main: green[100]
+        }
+    }
+
+    
+})
