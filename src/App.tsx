797425d0eab1f7666c505276ff8d220a2fb1d246
sg-i <sgadov2vanya@yandex.ru>
2021-08-04 17:49:02 +0300

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { Home } from './pages/Home/index';
 import { SignIn } from './pages/SignIn';
-
+//test commit
 function App() {
   return (
     <div className="App">
